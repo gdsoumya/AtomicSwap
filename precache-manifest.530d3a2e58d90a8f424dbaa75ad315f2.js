b9e68d3d546317fa28d270092dfbafc1024c92e4
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2020-09-15 19:49:53 +0530

    Updates

---
+++ precache-manifest.530d3a2e58d90a8f424dbaa75ad315f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e654122a467e098f151a722c12c07081",
+    "url": "/AtomicSwap/index.html"
+  },
+  {
+    "revision": "2c0c00274aa39630ca9e",
+    "url": "/AtomicSwap/static/css/main.ad95059a.chunk.css"
+  },
+  {
+    "revision": "64aecd1b6b13b9882cf2",
+    "url": "/AtomicSwap/static/js/2.f97c7bc4.chunk.js"
+  },
+  {
+    "revision": "91e9bfd1d08880b66e0e6b773fb8e781",
+    "url": "/AtomicSwap/static/js/2.f97c7bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c0c00274aa39630ca9e",
+    "url": "/AtomicSwap/static/js/main.ef39e663.chunk.js"
+  },
+  {
+    "revision": "52615379640f2177bfd5",
+    "url": "/AtomicSwap/static/js/runtime-main.d8fd0ecf.js"
+  }
+]);
